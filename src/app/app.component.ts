7e747ee791179d4413afc7267c6e10eaacc15579
balaoushamzah <balaoushamzah@gmail.com>
2020-05-31 14:11:18 +0000

    Version stable locale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appsGlobalisation';
+  title = 'appsGlobalisation';  
 }
